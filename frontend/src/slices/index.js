c350e9f746668b67836633f4df5092956fd4ee7b
NikoKrauche <K0lan@ya.ru>
2024-02-29 01:28:51 +0300

    modified:   frontend/src/slices/channelsSlice.js 	modified:   frontend/src/slices/index.js 	deleted:    frontend/src/slices/messagesSlice.js

--- frontend/src/slices/index.js
+++ frontend/src/slices/index.js
@@ -1,6 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './authorizationSlice.js';
-import messagesReducer from './messagesSlice.js';
 import channelsReducer from './channelsSlice.js';
 import modalReducer from './modalSlice.js';
 
@@ -9,7 +8,6 @@ import { chatApi } from '../services/chatApi.js';
 export default configureStore({
   reducer: {
     auth: authReducer,
-    messages: messagesReducer,
     channels: channelsReducer,
     modal: modalReducer,
     [chatApi.reducerPath]: chatApi.reducer,
